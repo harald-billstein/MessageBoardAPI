be9e49b5d3db51ef12c2cfa33ba8a7ba862151d2
Harald Billstein <harald.billstein@gmail.com>
2018-03-19 15:22:20 +0100

    Banned words now extracted to xml file

--- src/main/java/billstein/harald/chatApi/model/ProfanityFilter.java
+++ src/main/java/billstein/harald/chatApi/model/ProfanityFilter.java
@@ -1,24 +1,32 @@
 package billstein.harald.chatApi.model;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 @Data
 @Controller
 public class ProfanityFilter {
 
-  // IM AM SORRY!
-  private static List<String> badWords = new ArrayList<>(Arrays.asList(
-      "damn",
-      "dick",
-      "fuck"
-  ));
+  private static List<String> badWords;
   private Logger logger = LoggerFactory.getLogger(ProfanityFilter.class);
+
+  public ProfanityFilter() {
+    loadBannedWordsFromXML();
+  }
 
   public boolean isProfanityFree(String message) {
 
@@ -35,5 +43,32 @@ public class ProfanityFilter {
 
     }
     return messageClean;
+  }
+
+  private void loadBannedWordsFromXML() {
+
+    Resource resource = new ClassPathResource("profanity.xml");
+
+    try {
+      File xmlFile = resource.getFile();
+      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder dBuilder;
+
+      dBuilder = dbFactory.newDocumentBuilder();
+      Document doc = dBuilder.parse(xmlFile);
+
+      doc.getDocumentElement().normalize();
+      NodeList nodeList = doc.getElementsByTagName("word");
+
+      List<String> bannedWords = new ArrayList<>();
+      for (int i = 0; i < nodeList.getLength(); ++i) {
+        bannedWords.add(nodeList.item(i).getTextContent());
+      }
+      badWords = bannedWords;
+
+    } catch (IOException | ParserConfigurationException | SAXException e) {
+      e.printStackTrace();
+    }
+
   }
 }
