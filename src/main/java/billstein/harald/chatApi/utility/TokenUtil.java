71e1366a2b4229ee4b6311e93ace48ec29479fca
Harald Billstein <harald.billstein@gmail.com>
2018-03-29 00:09:11 +0200

    Method to check if token has expired or not added

--- src/main/java/billstein/harald/chatApi/utility/TokenUtil.java
+++ src/main/java/billstein/harald/chatApi/utility/TokenUtil.java
@@ -1,5 +1,7 @@
 package billstein.harald.chatApi.utility;
 
+import billstein.harald.chatApi.entity.UserEntity;
+import java.util.Calendar;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class TokenUtil {
@@ -14,5 +16,22 @@ public class TokenUtil {
 
   public static long generateTime() {
     return System.currentTimeMillis();
+  }
+
+  public static boolean hasTokenExpired(UserEntity userEntity) {
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTimeInMillis(userEntity.getWhenTokenWasCreated());
+
+    long createDate = userEntity.getWhenTokenWasCreated();
+    long currentTime = System.currentTimeMillis();
+    long oneDay = 86400000;
+    //long tenSeconds = 10000;
+
+    long tokenAge = oneDay + createDate;
+    long result = currentTime - tokenAge;
+
+    System.out.println(result);
+
+    return result >= 0;
   }
 }
