0676b86d4bc0929000d72b23998b4034b309a814
Harald Billstein <harald.billstein@gmail.com>
2018-04-09 08:04:04 +0200

    typo

---
+++ src/test/java/billstein/harald/chatApi/service/AdminServiceControllerTest.java
@@ -0,0 +1,56 @@
+package billstein.harald.chatApi.service;
+
+import billstein.harald.chatApi.ChatApiApplication;
+import billstein.harald.chatApi.entity.UserEntity;
+import billstein.harald.chatApi.handlers.UserHandler;
+import billstein.harald.chatApi.model.IncomingProfanityRequest;
+import billstein.harald.chatApi.model.OutgoingProfanityRequest;
+import billstein.harald.chatApi.persistent.ProfanityIO;
+import billstein.harald.chatApi.service.AdminServiceController;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ChatApiApplication.class)
+public class AdminServiceControllerTest {
+
+  @Autowired
+  private AdminServiceController adminServiceController;
+  @Autowired
+  private UserHandler userHandler;
+  @Autowired
+  private ProfanityIO profanityIO;
+
+  @Test
+  public void profanityIOTest() {
+
+    UserEntity userEntity = userHandler.getUser("Harald");
+
+    IncomingProfanityRequest request = new IncomingProfanityRequest();
+    request.setWord("testWord");
+    request.setUserName(userEntity.getUserName());
+    request.setToken(userEntity.getToken());
+
+    ResponseEntity<OutgoingProfanityRequest> response = adminServiceController
+        .addBannedWord(request);
+
+    List<String> words = profanityIO.loadBannedWordsFromXML();
+
+    Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+    Assert.assertEquals(request.getWord(), words.get((words.size() - 1)));
+
+    adminServiceController.removeBannedWord(request);
+    words = profanityIO.loadBannedWordsFromXML();
+
+    Assert.assertNotEquals(request.getWord(), words.get((words.size() - 1)));
+
+  }
+
+}
